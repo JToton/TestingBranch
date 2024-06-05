1d46c0bd419098787d9b9faa8607f7886a39eacf
DallasGiles <105821196+DallasGiles@users.noreply.github.com>
2024-06-04 19:31:37 -0600

    added base to some js

---
+++ controllers/eventController.js
@@ -0,0 +1,9 @@
+const { Event } = require('../models');
+
+exports.createEvent = async (req, res) => {
+  // Event creation logic
+};
+
+exports.getEvents = async (req, res) => {
+  // Get events logic
+};
